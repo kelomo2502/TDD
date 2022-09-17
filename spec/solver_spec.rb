20d90129f2833ed345d615836be00172ea4b20cc
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-17 03:59:31 +0100

    Add the factorial method tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,3 +5,21 @@ context 'Given a string' do
     expect(word.reverse_str('hello')).to eq('olleh')
   end
 end
+
+describe Solver do
+  before :each do
+    @solve = Solver.new
+  end
+
+  it "Testing the 'factorial' method" do
+    expect(@solve.factorial(4)).to eq(24)
+  end
+
+  it 'Testing the factorial of 0' do
+    expect(@solve.factorial(0)).to eq(1)
+  end
+
+  it 'Testing the factorial of negative number' do
+    expect { @solve.factorial(-4) }.to raise_exception
+  end
+end
