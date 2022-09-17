9961059384bb6a9cf883a7904675897bf4c734e7
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-17 04:00:18 +0100

    Add fixbuzz test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -23,3 +23,25 @@ describe Solver do
     expect { @solve.factorial(-4) }.to raise_exception
   end
 end
+
+describe "Testing the 'fizzbuzz' method" do
+  before :each do
+    @test = Solver.new
+  end
+
+  it "Expecting 3 to return 'fizz'" do
+    expect(@test.fizzbuzz(3)).to eq('fizz')
+  end
+
+  it "Expecting 5 to return 'buzz'" do
+    expect(@test.fizzbuzz(5)).to eq('buzz')
+  end
+
+  it "Expecting 15 to return 'fizzbuzz'" do
+    expect(@test.fizzbuzz(15)).to eq('fizzbuzz')
+  end
+
+  it "Expecting 7 to return '7'" do
+    expect(@test.fizzbuzz(7)).to eq('7')
+  end
+end
